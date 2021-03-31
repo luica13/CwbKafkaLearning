623f86de40784850f49d37840f8f33b5af60296d
Asatur Meltonyan <asatur.meltonyan@webbfontaine.com>
2021-03-31 15:43:22 +0400

    Create kafka producer and send the data.

--- src/main/java/com/webbfontain/kafka/tutorial1/TwitterProducer.java
+++ src/main/java/com/webbfontain/kafka/tutorial1/TwitterProducer.java
@@ -1,6 +1,7 @@
 package com.webbfontain.kafka.tutorial1;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -15,6 +16,10 @@ import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 @Slf4j
 public class TwitterProducer {
@@ -23,6 +28,9 @@ public class TwitterProducer {
     private static final String API_SECRET = "ZUA1qjCuGUsmMWSm7WhAq0CBOfg1Q3gnNb5bwvBWxkQI6NBlWk";
     private static final String ACCESS_TOKEN = "2611730924-RNkNKJdfvNdTr9v7GiBC2TG9TbKlBdZuik9fxLR";
     private static final String ACCESS_TOKEN_SECRET = "TyT6qOpUukQhpq67unCLzFG8euZkG7mxpcNDHw8h2ohN2";
+
+    private static final String BOOTSTRAP_SERVER = "127.0.0.1:9092";
+    private static final String TOPIC_NAME = "twitter_tweets";
 
     public static void main(String[] args) {
         new TwitterProducer().run();
@@ -36,16 +44,24 @@ public class TwitterProducer {
         Client client = createTwitterClient(msgQueue);
         client.connect();
 
+        KafkaProducer<String, String> kafkaProducer = createKafkaProducer();
+
         while (!client.isDone()) {
             String msg = null;
             try {
                 msg = msgQueue.poll(5, TimeUnit.SECONDS);
+                docker
             } catch (InterruptedException e) {
                 log.error("Error during poll", e);
                 client.stop();
             }
             if (msg != null) {
                 log.info("message: {}", msg);
+                kafkaProducer.send(new ProducerRecord<>(TOPIC_NAME, null, msg), (metadata, e) -> {
+                    if (null != e) {
+                        log.error("Smth wrong happened: ", e);
+                    }
+                });
             }
         }
         log.info("End of application");
@@ -67,5 +83,14 @@ public class TwitterProducer {
             .processor(new StringDelimitedProcessor(msgQueue));
 
         return builder.build();
+    }
+
+    public KafkaProducer<String, String> createKafkaProducer() {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return new KafkaProducer<>(properties);
     }
 }
